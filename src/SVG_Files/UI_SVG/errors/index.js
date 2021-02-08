357c6e2c1238678e3b009ff673f28356a2c68ce4
Ray <madebyraymond@gmail.com>
2021-02-09 00:14:29 +0100

    Error SVG files

---
+++ src/SVG_Files/UI_SVG/errors/index.js
@@ -0,0 +1,16 @@
+import noArchive from './noArchive';
+import noInternet from './noInternet';
+import noSaved from './noSaved';
+import noWishlist from './noWishlist';
+import wishlistDetailsError from './wishlistDetailsError';
+import wishlistNotExist from './wishlistNotExist';
+
+
+export {
+    noArchive,
+    noInternet,
+    noSaved,
+    noWishlist,
+    wishlistDetailsError,
+    wishlistNotExist
+}
