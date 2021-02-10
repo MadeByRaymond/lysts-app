8dd642e73ca7e2354a50c594c9bd73c4b38a195c
Ray <madebyraymond@gmail.com>
2021-02-10 07:06:27 +0100

    save icon svg render management

--- src/components/saveWishlistButton/saveButton.js
+++ src/components/saveWishlistButton/saveButton.js
@@ -2,17 +2,35 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import * as SaveIcon from '../../SVG_Files/saveSVG/saveSVG';
+import {Touchable} from '../../includes/variables';
 
 const saveButton = (props) => {
-    let Icon =  props.saved ? SaveIcon.Saved : SaveIcon.Unsaved;
+    let Icon = props.savedStatus == 'loading' ? SaveIcon.Loading :  props.savedStatus == 'saved' ? SaveIcon.Saved : SaveIcon.Unsaved;
+    // if(props.savedStatus == 'saved'){
+    //     Icon = SaveIcon.Saved
+    // }else if(props.savedStatus == 'unsaved'){
+    //     Icon = SaveIcon.Unsaved
+    // }else if(props.savedStatus == 'loading'){
+    //     Icon = SaveIcon.Unsaved
+    // }
+    let TouchableView  = props.savedStatus == 'loading' ? View : Touchable;
+
     let color = props.color ? props.color : '#E76666';
     return (
-        <Icon height={props.height} width={props.width} color={color} />
+        <View style={styles.container}>
+            <TouchableView hitSlop={{top:20, left:20, bottom:20, right:20}} onPress = {() => {props.saveFunc ? props.saveFunc() : null}}>
+                <Icon height={props.height} width={props.width} color={color} />
+            </TouchableView>
+        </View>
+        
     )
 }
 
 export default saveButton
 
 const styles = StyleSheet.create({
-
+    container:{
+        borderRadius: 5,
+        overflow: 'hidden'
+    }
 })
