cd3f4005100af0df0d299d2c819bf6b6d3c4c771
Ray <madebyraymond@gmail.com>
2021-02-11 07:57:43 +0100

    App.js and Package.json files

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-
+import { AppRegistry, Platform, Alert, Linking } from 'react-native';
 import { Navigation } from "react-native-navigation";
 import {Provider} from 'react-redux';
 import configureStore from './src/store/config';
+import PushNotification from "react-native-push-notification";
 
+import {message, inAppMessaging} from './src/services/FCMService';
 import {app as realmApp} from './storage/realm';
 
 import AsyncStorage from '@react-native-community/async-storage';
@@ -15,6 +17,15 @@ if(__DEV__) {
 // REDUX STORE 
 const store = configureStore();
 
+// SPLASH SCREENS IMPORTS
+import Splash from "./src/screens/splashScreen/splashScreen";
+
+// ONBOARDING SCREENS 
+import OnBoarding from "./src/screens/onBoarding/onboarding";
+
+// AUTH SCREENS IMPORTS
+import Auth from "./src/screens/Auth/auth";
+
 // APP SCREENS IMPORTS
 import Wishlist from "./src/screens/wishlist/wishlist";
 import Home from "./src/screens/home/home";
@@ -23,12 +34,13 @@ import SelectCategory from "./src/screens/createWishlist/selectCategory";
 import WishlistDetails from "./src/screens/wishlistDetails/wishlistDetails";
 import Profile from "./src/screens/profile/profile";
 import ProfileInfo from "./src/screens/profile/profileInfo";
+import CustomizeAvatar from "./src/screens/profile/customizeAvatar";
 import Save_Archive from  "./src/screens/wishlist/save_archive";
 import Settings from  "./src/screens/settings/settings";
 import AboutSettings from  "./src/screens/settings/about_settings";
 import NotificationSettings from  "./src/screens/settings/notification_settings";
 import SecuritySettings from  "./src/screens/settings/security_settings";
-import Auth from "./src/screens/Auth/auth";
+
 // APP COMPONENTS IMPORTS
 import SavedButton from "./src/components/saveWishlistButton/saveButton";
 
@@ -36,6 +48,11 @@ import SavedButton from "./src/components/saveWishlistButton/saveButton";
 import Demo from "./src/screens/demo";
 Navigation.registerComponent('com.lysts.screen.demo', () => Demo);
 
+// SPLASH SCREEN
+Navigation.registerComponent('com.lysts.screen.splash', () => Splash);
+
+// ONBOARDING SCREEN
+Navigation.registerComponent('com.lysts.screen.onboarding', () => OnBoarding);
 
 // AUTH SCREEN
 Navigation.registerComponentWithRedux('com.lysts.screen.auth', () => Auth, Provider, store);
@@ -47,7 +64,8 @@ Navigation.registerComponent('com.lysts.screen.selectCategory', () => SelectCate
 Navigation.registerComponentWithRedux('com.lysts.screen.createWishlist', () => CreateWishlist, Provider, store);
 Navigation.registerComponent('com.lysts.screen.wishlistDetails', () => WishlistDetails);
 Navigation.registerComponent('com.lysts.screen.profile', () => Profile);
-Navigation.registerComponentWithRedux('com.lysts.screen.profileInfo', () => ProfileInfo, Provider, store);
+Navigation.registerComponent('com.lysts.screen.profileInfo', () => ProfileInfo);
+Navigation.registerComponent('com.lysts.screen.customizeAvatar', () => CustomizeAvatar);
 Navigation.registerComponentWithRedux('com.lysts.screen.save_archive', () => Save_Archive, Provider, store);
 Navigation.registerComponentWithRedux('com.lysts.screen.settings', () => Settings, Provider, store);
 Navigation.registerComponent('com.lysts.screen.aboutSettings', () => AboutSettings);
@@ -56,20 +74,124 @@ Navigation.registerComponentWithRedux('com.lysts.screen.securitySettings', () =>
 // NON SCREEN COMPONENTS REGISTER
 Navigation.registerComponent('com.lysts.component.SavedButton', () => SavedButton);
 
+// HEADLESS JS COMPONENT REGISTER 
+// if(Platform.OS === 'android'){
+//   AppRegistry.registerHeadlessTask('BackgroundNotification', () => require('./BackgroundNotification'))
+// }
 
+// alert('ddd')
 // REQUIRED APP RESOURCES
-require("./src/assets/images/home_bg.png");
+// require("./src/assets/images/home_bg.png");
+// Register background handler
+message.setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+message.onMessage(async remoteMessage => {
+  // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+  (remoteMessage.data.isNotification == "true" && remoteMessage.data.showInForeground == "true") 
+  ? PushNotification.localNotification({
+    channelId: remoteMessage.notification.android.channelId,
+    autoCancel: false,
+    bigText: remoteMessage.notification.body,
+    // subText: remoteMessage.notification.body,
+    title: remoteMessage.notification.title,
+    message: remoteMessage.notification.body,
+    largeIconUrl: remoteMessage.notification.android.imageUrl,
+    bigPictureUrl: remoteMessage.notification.android.imageUrl,
+    smallIcon: "ic_launcher_transparent",
+    priority: "high",
+    // color: "blue",
+    showWhen: true,
+    // when: new Date(),
+    vibrate: true,
+    vibration: 300,
+    playSound: true,
+    // soundName: 'notification_sound.mp3',
+    // actions: ["Cancel"],
+  }) : null
+});
+message.subscribeToTopic('general').then(() => console.log('Subscribed to topic!'));
+message.subscribeToTopic('app_updates').then(() => console.log('Subscribed to topic!'));
+
+message.onNotificationOpenedApp(remoteMessage => {
+  console.log(
+    'Notification caused app to open from background state:',
+    remoteMessage.notification,
+  );
+  remoteMessage.data.redirectURL ? Linking.openURL(remoteMessage.data.redirectURL) : null
+  // navigation.navigate(remoteMessage.data.type);
+});
+
+// Check whether an initial notification is available
+message
+  .getInitialNotification()
+  .then(remoteMessage => {
+    if (remoteMessage) {
+      console.log(
+        'Notification caused app to open from quit state:',
+        remoteMessage.notification,
+      );
+      remoteMessage.data.redirectURL ? Linking.openURL(remoteMessage.data.redirectURL) : null
+      // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+    }
+    // setLoading(false);
+  });
 
 
+inAppMessaging().setMessagesDisplaySuppressed(false);
+
+export const splashRoot = {
+  root: {
+    component: {
+      name: 'com.lysts.screen.splash',
+      options : {
+        statusBar: {
+          backgroundColor: 'transparent',
+          drawBehind: true,
+          translucent: true,
+          animate: true,
+          blur: true,
+          style: 'dark'
+        }
+      }
+    }
+  }
+};
+
+export const onBoardingRoot = {
+  root: {
+    component: {
+      name: 'com.lysts.screen.onboarding',
+      options : {
+        statusBar: {
+          backgroundColor: 'transparent',
+          drawBehind: true,
+          translucent: true,
+          animate: true,
+          blur: true,
+          style: 'dark'
+        }
+      }
+    }
+  }
+};
 
 export const loginRoot = {
   root: {
-    component: {
-      name: 'com.lysts.screen.auth',
+    stack: {
+      id: 'AUTH_STACK',
+      children: [
+        {
+          component: {
+            id: 'AUTH_SCREEN',
+            name: 'com.lysts.screen.auth'
+          }
+        }
+      ],
       options : {
         statusBar: {
-          backgroundColor: '#000',
-          style: 'dark'
+          backgroundColor: '#FEFEFD',
+          style: 'dark',
         }
       }
     }
@@ -213,6 +335,15 @@ Navigation.setDefaultOptions({
     elevation: 15
   },
   animations:{
+    setRoot :{
+      alpha:{
+        from : 0,
+        to: 1,
+        duration: 400,
+        startDelay: 0,
+        interpolation: 'decelerate'
+      },
+    },
     push:{
       content:{
         // y:{
@@ -238,15 +369,30 @@ Navigation.setDefaultOptions({
 });
 
 Navigation.events().registerAppLaunchedListener(async() => {
-   Navigation.setRoot(await isLoggedIn() ? mainRoot : loginRoot);
-  //  Navigation.setRoot(mainRoot);
+  //  Navigation.setRoot(await isLoggedIn() ? mainRoot : loginRoot);
+  //  Navigation.setRoot(await getRoot());
+  Navigation.setRoot(splashRoot);
 });
 
-const isLoggedIn = async () =>{
+export const getRoot = async () =>{
   try {
-    return (realmApp.currentUser.isLoggedIn) ? true : false;
+    let navRoot;
+    if((realmApp.currentUser !== null) && (typeof realmApp.currentUser !== 'undefined') && (realmApp.currentUser.isLoggedIn && realmApp.currentUser.providerType != 'anon-user')){
+      navRoot = mainRoot;
+    }else{
+      console.log('diff, ');
+      let lastOpenedDate = await AsyncStorage.getItem('lystsApp:appStorage:lastOpenedDate');
+      console.log('diff, ', lastOpenedDate);
+      let hours = Math.abs(Math.floor(((new Date().getTime()) - (new Date(lastOpenedDate).getTime())) / (60*60*1000)));
+      navRoot = (hours > 1) ? onBoardingRoot : loginRoot;
+    }
+
+    await AsyncStorage.setItem('lystsApp:appStorage:lastOpenedDate', `${new Date()}`);
+    return navRoot;
   } catch(e) {
-    return false;
+    console.log(e);
+    await AsyncStorage.setItem('lystsApp:appStorage:lastOpenedDate', `${new Date()}`);
+    return onBoardingRoot;
   }
 
 
@@ -281,3 +427,11 @@ const isLoggedIn = async () =>{
   //   return false;
   // }
 }
+
+
+
+
+
+
+
+// BackgroundNotification
