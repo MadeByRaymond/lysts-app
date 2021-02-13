06034e48bda4c5b12bf547202256684b931b39a2
Ray <madebyraymond@gmail.com>
2021-02-13 11:10:34 +0100

    code optimization

--- storage/realm.js
+++ storage/realm.js
@@ -17,34 +17,34 @@ export function getRealmApp() {
 
 export let app = getRealmApp();
 
-class Task {
-    constructor({
-      name,
-      partition,
-      status = Task.STATUS_OPEN,
-      id = new ObjectId(),
-    }) {
-      this._partition = partition;
-      this._id = id;
-      this.name = name;
-      this.status = status;
-    }
+// class Task {
+//     constructor({
+//       name,
+//       partition,
+//       status = Task.STATUS_OPEN,
+//       id = new ObjectId(),
+//     }) {
+//       this._partition = partition;
+//       this._id = id;
+//       this.name = name;
+//       this.status = status;
+//     }
   
-    static STATUS_OPEN = 'Open';
-    static STATUS_IN_PROGRESS = 'InProgress';
-    static STATUS_COMPLETE = 'Complete';
+//     static STATUS_OPEN = 'Open';
+//     static STATUS_IN_PROGRESS = 'InProgress';
+//     static STATUS_COMPLETE = 'Complete';
   
-    static schema = {
-      name: 'Task',
-      properties: {
-        _id: 'object id',
-        _partition: 'string',
-        name: 'string',
-        status: 'string',
-      },
-      primaryKey: '_id',
-    };
-  }
+//     static schema = {
+//       name: 'Task',
+//       properties: {
+//         _id: 'object id',
+//         _partition: 'string',
+//         name: 'string',
+//         status: 'string',
+//       },
+//       primaryKey: '_id',
+//     };
+//   }
 
   export async function anonymousLogin() {
     let user;
