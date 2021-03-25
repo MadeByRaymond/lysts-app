c436f5e99e74fb8d81381a09ddeb34c6dd1d4e65
Ray <madebyraymond@gmail.com>
2021-03-25 22:40:43 +0100

    Final Instructions

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ PushNotification.createChannel();
 
 require('./App');
 
+// STEPS 
+/*
+1. Save settings in async storage
+2. On load(After Login), check if logout after x time(settings) is not infinite
+3. If not infinite, check last logged in is greater than logout after x value
+4. if greater, show modal saying session expired. action button logs user out
+*/
